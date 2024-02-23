62190966051ac4a8b43da4a41cd8359123c4dff2
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2024-02-24 08:38:23 +0900

    SlackMessenger class for python

---
+++ python_utils/slack_messenger.py
@@ -0,0 +1,17 @@
+import json
+
+import requests
+
+
+class SlackMessenger:
+    def send_message(self, url: str, message_dict: str):
+        header = {"Content-type": "application/json"}
+        connect_timeout = 5.0
+        read_timeout = 30.0
+
+        message = json.dumps(message_dict)
+
+        response = requests.post(
+            url, data=message, headers=header, timeout=(connect_timeout, read_timeout)
+        )
+        response.raise_for_status()
